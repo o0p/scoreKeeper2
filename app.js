b1b6d7f50e00ca21f8abcb030292fd86a667a0c4
Russell <russelldev@protonmail.com>
2020-12-10 09:33:51 +0000

    Add app.js - refactor 4

--- app.js
+++ app.js
@@ -50,12 +50,10 @@ resetButton.addEventListener('click', reset);
 
 function reset() {
     isGO = false;
-    p1.score = 0;
-    p2.score = 0;
-    p1.display.textContent = 0;
-    p2.display.textContent = 0;
-    p1.display.classList.remove('has-text-success','has-text-danger');
-    p2.display.classList.remove('has-text-danger','has-text-success');
-    p1.button.disabled = false;
-    p2.button.disabled = false;
+    for (let p of [p1, p2]) {
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('has-text-success', 'has-text-danger');
+        p.button.disabled = false;
+    }
     };
